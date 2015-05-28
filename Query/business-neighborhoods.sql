8dd93a78d59d6ae977fbd71777bf0b6cd613b5fd
MangustLP <olly.pilat@gmail.com>
2015-05-28 13:08:23 +0200

    .sql

---
+++ Query/business-neighborhoods.sql
@@ -0,0 +1,17 @@
+﻿CREATE TABLE "Progetto"."business-neighborhoods"
+(
+  record_type character(8),
+  business_id character(22) NOT NULL,
+  name character(100),
+  city character(20),
+  state character(3),
+  latitude double precision,
+  longitude double precision,
+  neighborhood character(30) NOT NULL,
+  CONSTRAINT "business-neighborhoods_pkey" PRIMARY KEY (business_id, neighborhood)
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE "Progetto"."business-neighborhoods"
+  OWNER TO postgres;
---
+++ Query/user-profiles.sql
@@ -0,0 +1,17 @@
+﻿CREATE TABLE "Progetto"."user-profiles"
+(
+  record_type character(4),
+  user_id character(22) NOT NULL,
+  name character(30),
+  review_count smallint,
+  average_stars real,
+  registered_on character(7),
+  fans_count integer,
+  elite_years_count smallint,
+  CONSTRAINT "user-profiles_pkey" PRIMARY KEY (user_id)
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE "Progetto"."user-profiles"
+  OWNER TO postgres;
