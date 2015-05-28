825bff6f286bbea3012a9cca45859800ce613d74
Sadga <alescussel@tiscali.it>
2015-05-28 11:24:12 +0200

    nuove query

---
+++ Query/user-friends.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE "Progetto"."user-friends"
+(
+  record_type character(6),
+  user_id character(22) NOT NULL,
+  name character(30),
+  friend_id character(22) NOT NULL,
+  CONSTRAINT "user-friends_pkey" PRIMARY KEY (user_id, friend_id)
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE "Progetto"."user-friends"
+  OWNER TO postgres;
