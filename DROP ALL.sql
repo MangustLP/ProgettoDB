8e8e109834b6ff9654b55d27267b5b050293fe21
Sadga <alescussel@tiscali.it>
2015-06-11 17:22:42 +0200

    drop delle tabelle e dello schema

---
+++ DROP ALL.sql
@@ -0,0 +1,10 @@
+﻿DROP TABLE "Progetto"."business-openhours";
+DROP TABLE "Progetto"."business-categories";
+DROP TABLE "Progetto"."business-neighborhoods";
+DROP TABLE "Progetto"."user-profiles";
+DROP TABLE "Progetto"."user-votes";
+DROP TABLE "Progetto"."user-compliments";
+DROP TABLE "Progetto"."review-votes";
+DROP TABLE "Progetto"."reviews";
+DROP TABLE "Progetto"."user-friends";
+DROP SCHEMA "Progetto"
