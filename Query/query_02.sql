56171e615f158619c9a031937d4be27a7c2ba59a
mangustLP <olly.pilat@gmail.com>
2015-06-12 15:22:01 +0200

    done

--- Query/query_02.sql
+++ Query/query_02.sql
@@ -1,12 +1,18 @@
 ﻿SELECT B1.business_id ,B1.review_count, B1.stars, B1.city, B2.business_id as business_id2,B2.review_count as review_count2, B2.stars as stars2, B2.city as city2
 
-FROM "Progetto"."business-categories" as B1 INNER JOIN "Progetto"."business-categories" as B2 ON B1.category =  B2.category
+FROM (SELECT *
 
-WHERE B1.city <> B2.city AND B2.review_count = (SELECT max(review_count) 
+      FROM "Progetto"."business-categories" 
 
-						FROM "Progetto"."business-categories" as B3
+      WHERE "Progetto"."business-categories".open = 'True') as B1 INNER JOIN (SELECT *
 
-						WHERE B3.category = B1.category)
-ORDER BY business_id
+									      FROM "Progetto"."business-categories"
+
+									      WHERE "Progetto"."business-categories".open = 'True') as B2 ON B1.category =  B2.category
+
+WHERE B1.city <> B2.city AND B2.review_count < B1.review_count AND B2.stars < B1.stars
+
+GROUP BY B1.business_id ,B1.review_count, B1.stars, B1.city, business_id2,review_count2, stars2, city2
+
 					
 
